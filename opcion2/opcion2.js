026ebe1f530b09d895adc90557abd5a596a8934f
MasamioNakada <mnakadab@unac.edu.pe>
2021-09-27 00:48:37 -0500

    second commit

--- opcion2/opcion2.js
+++ opcion2/opcion2.js
@@ -1,29 +1,42 @@
-let  hamburguesa = {
-    simple: 20,
-    doble: 25,
-    triple: 30,
+
+var método = ["efectivo", "crédito"]
+
+var cod = prompt(`Seleccione que hamburguesa Comprará: Simple, Doble, Triple.
+Cuantas hamburguesas deseará?.
+Mencionenos también el método de pago: Efectivo , Credito`)
+
+
+
+if (cod.toLowerCase().includes("simple")){
+    var ti = "simple"
+}else if(cod.toLowerCase().includes("doble")){
+    var ti = "doble"
+}else if (cod.toLowerCase().includes("triple")){
+    var ti = "triple"
 }
 
-var cod = prompt("Seleccione que hamburguesa Comprará: Simple, Doble, Triple")
+switch (ti) {
+    case "simple":
+        
+        var n = parseInt(cod.includes())
+/*         var to = n * 20
+        var tot = n * 20 + 0.05*to */
+        /* if (tp.toLowerCase().includes('efectivo')){ }*/
 
-switch (cod) {
-    case 1:
-        var ti = hamburguesa.simple
-        var n = parseInt(prompt(`Cuantas hamburguesas ${hamburguesa[0]} ordenará `))
-        var to = n * ti
-        var tot = n * ti + 0.05*to
+        console.log(n)
+
         break;
-    case 2:
+    case "doble":
         var ti = hamburguesa.doble
         var n = parseInt(prompt(`Cuantas hamburguesas ${hamburguesa[1]} ordenará `))
-        var to = n * ti
-        var tot = n * ti + 0.05*to
+        var to = n * 25
+        var tot = n * 25 + 0.05*to
         break;
-    case 3:
+    case "triple":
         var ti = hamburguesa.triple
         var n = parseInt(prompt(`Cuantas hamburguesas ${hamburguesa[2]} ordenará `))
-        var to = n * ti
-        var tot = n * ti + 0.05*to
+        var to = n * 30
+        var tot = n * 30 + 0.05*to
         break;
                 
     default:
